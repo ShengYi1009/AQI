a1fb4b1f1d3980d888c161c14b2e7a8d2c06cd1f
4b3g0132@office.stust.edu.tw <4b3g0132@office.stust.edu.tw>
2025-12-11 14:46:21 +0800

    重構主視窗並新增 AQI 資料結構
    重構 MainWindow.xaml 及其後端程式，擴充 UI 元件，加入 DataGrid、圖表等，並改善欄位命名與排版。新增 AQIdata.cs，定義完整 AQI 資料類別以對應 API JSON。實作非同步取得與解析 AQI 資料，更新狀態列顯示資料筆數，為後續資料呈現與圖表功能奠定基礎。

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,6 +8,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -16,9 +19,51 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        String aqiURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=4c89a32a-a214-461b-bf29-30ff32a61a8a&limit=1000&sort=ImportDate%20desc&format=JSON";
+
+        AQIData aqiData = new AQIData();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
+
         public MainWindow()
         {
             InitializeComponent();
+            URLTextBox.Text = aqiURL;
+        }
+
+        private async void GetAqiButton_Click(object sender, RoutedEventArgs e)
+        {
+            string url = URLTextBox.Text;
+            ContentTextBox.Text = "抓取資料中......";
+
+            String data = await GetAQIAync(url);
+            if (data != null)
+            {
+                ContentTextBox.Text = data;
+                aqiData = JsonSerializer.Deserialize<AQIData>(data);
+                fields = aqiData.fields;
+                records = aqiData.records;
+                StatusTextBlock.Text = $"取得資料成功，共有 {records.Count} 筆記錄。";
+            }
+
+        }
+
+        private async Task<string> GetAQIAync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    StatusTextBlock.Text = "無法取得資料，HTTP Status: " + response.StatusCode;
+                    return null;
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+            }
         }
     }
 }
